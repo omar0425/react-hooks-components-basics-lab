f7e2c92cb618fae591523e60346eb708157da786
Omar Razi <startfresh87@gmail.com>
2022-01-31 13:17:09 -0500

    commit tree b517556d19748ff8128a39af82d33c795d18f120

--- src/components/App.js
+++ src/components/App.js
@@ -3,28 +3,33 @@ import React from "react";
 function NavBar() {
   return (
     <nav>
-      <a href="#home">I'm a link!</a>
+      <a href='#home'>I'm a link!</a>
     </nav>
   );
 }
 
 function Home() {
   return (
-    <div id="home">
+    <div id='home'>
       <h1>Home</h1>
     </div>
   );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id='about'>
+      <h2>About</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
